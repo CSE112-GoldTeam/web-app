2b0b0080ad47c6a15627ffdda9340814014803b6
J Delaney <j.delaney.sd@gmail.com>
2015-01-27 10:24:30 -0800

    Insert MongoDB and Monk. Includes middleware to allow routers to access db.

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//Database
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/robobetty');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +26,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+app.use(function(req, res, next){
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
