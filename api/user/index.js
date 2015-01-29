13e8b2f675afd7f7db9fe0ed6f401b8d05f1af71
Alex Gian <alexhgian@gmail.com>
2015-01-29 15:05:58 -0800

    Most REST api calls works

--- api/user/index.js
+++ api/user/index.js
@@ -6,10 +6,10 @@ var controller = require('./user.controller');
 var router = express.Router();
 
 router.get('/', controller.index);
-// router.get('/:id', controller.show);
-// router.post('/', controller.create);
+router.get('/:id', controller.show);
+router.post('/', controller.create);
 // router.put('/:id', controller.update);
 // router.patch('/:id', controller.update);
-// router.delete('/:id', controller.destroy);
+router.delete('/:id', controller.destroy);
 
 module.exports = router;
