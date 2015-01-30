b67d42741f3ee2a2faeef09c19bc2582c6792b5f
Sergio Leon <sergio@debian>
2015-01-30 15:54:07 -0800

    Added HTML for welcome page.

--- routes/checkin.js
+++ routes/checkin.js
@@ -6,4 +6,12 @@ router.get('/sample2', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
+router.get('/walkin', function (req, res, next) {
+    res.render('walkin', {title: 'Express'});
+});
+
+router.get('/welcome', function (req, res, next) {
+    res.render('welcome', {title: 'Express'});
+});
+
 module.exports = router;
