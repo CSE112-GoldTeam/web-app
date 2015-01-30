48e7f531093ee4b23d47bf98eb3e389d00921a51
Alex Gian <alexhgian@gmail.com>
2015-01-30 15:18:03 -0800

    Enter Code view working

--- routes/code.js
+++ routes/code.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/code', function (req, res, next) {
     //res.send("Hello Code");
-    res.render('code', {title: 'Code'});
+    res.render('code', {title: 'CompanyName'});
 });
 
 module.exports = router;
