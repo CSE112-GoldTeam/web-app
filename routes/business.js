b5a2678ab895b464f1b9228a99a38d55b5d8448f
Giovanni Wong <deargiovanni89@gmail.com>
2015-01-30 17:35:08 -0800

    registration page finished

--- routes/business.js
+++ routes/business.js
@@ -6,4 +6,8 @@ router.get('/sample', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
+router.get('/registration', function (req, res, next) {
+    res.render('registration');
+});
+
 module.exports = router;
