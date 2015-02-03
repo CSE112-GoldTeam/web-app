acf9e57dfc6877f28432c7b409c031da0a16c428
Sergio Leon <sergio@debian>
2015-02-02 18:56:03 -0800

    Fixed another merge error

--- routes/business.js
+++ routes/business.js
@@ -6,13 +6,11 @@ router.get('/sample', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
-<<<<<<< HEAD
 router.get('/manageForms', function (req, res, next) {
     res.render('manageForms', {title: 'Express'});
-=======
+
 router.get('/settings', function (req, res, next) {
     res.render('settings', {title: 'Express'});
->>>>>>> develop
 });
 
 module.exports = router;
