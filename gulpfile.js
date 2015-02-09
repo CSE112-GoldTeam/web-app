97d977942406b18246350696568b12d495467316
Timothy Kua <timothyk7@gmail.com>
2015-02-08 16:37:43 -0800

    initial

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+// gulpfile.js
+var gulp = require('gulp');
+var server = require('gulp-express');
+
+gulp.task('server', function () {
+    // Start the server at the beginning of the task
+    server.run({
+        file: './bin/www'
+    });
+
+    // Restart the server when file changes
+    gulp.watch(['app/**/*.html'], server.notify);
+    gulp.watch(['app/styles/**/*.scss'], ['styles:scss']);
+    //gulp.watch(['{.tmp,app}/styles/**/*.css'], ['styles:css', server.notify]);
+    //Event object won't pass down to gulp.watch's callback if there's more than one of them.
+    //So the correct way to use server.notify is as following:
+    gulp.watch(['{.tmp,app}/styles/**/*.css'], function(event){
+        gulp.run('styles:css');
+        server.notify(event);
+        //pipe support is added for server.notify since v0.1.5, 
+        //see https://github.com/gimm/gulp-express#servernotifyevent
+    });
+
+    gulp.watch(['app/scripts/**/*.js'], ['jshint']);
+    gulp.watch(['app/images/**/*'], server.notify);
+    gulp.watch(['app.js', 'routes/**/*.js'], [server.run]);
+});
+
+gulp.task('default', ['server']);
+
+ 
+var karma = require('karma').server;
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);
+});
