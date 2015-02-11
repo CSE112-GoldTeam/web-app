e7ead95e33d4f7bc23b3f7c2fc73a9c05da14a01
Giovanni Wong <deargiovanni89@gmail.com>
2015-02-10 17:16:03 -0800

    working on local auth

--- app.js
+++ app.js
@@ -5,19 +5,34 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+//login config
+var passport = require('passport');
+var flash = require('connect-flash');
+
+
+
 //Database
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/robobetty');
 
-var business = require('./routes/business');
-var checkin = require('./routes/checkin');
+//var db =monk('localhost:27017/login_test');
+
+
+
+
+
+
+//var business = require('./routes/business');
+//var checkin = require('./routes/checkin');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -27,14 +42,34 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//required for passport
+app.use(session({secret: 'test'}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+
+
 // Make our db accessible to our router
 app.use(function (req, res, next) {
     req.db = db;
     next();
 });
 
-app.use('/', business);
-app.use('/', checkin);
+
+
+//uncomment after trying passportjs
+/*app.use('/', business);
+app.use('/', checkin);*/
+
+
+
+
+ // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app, passport); 
+
+// require('./config/passport')(passport); // pass passport for configuration
 
 
 // catch 404 and forward to error handler
