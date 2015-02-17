06ca5d01061ae228f20592a1044dc0185bd7e0c5
Hubert Le <hhl028@ucsd.edu>
2015-02-17 10:28:48 -0800

    Finished Sig-gen

--- routes/signature.js
+++ routes/signature.js
@@ -1,10 +1,27 @@
-var express = require('express');
+var express = require('express'),
+    fs = require('fs'),
+    gm = require('gm');
 var router = express.Router();
 
 router.get('/api/signature/:text', function (req, res, next) {
     var text = req.params.text;
 
-    res.send(text);
+    var exec = require('child_process').exec,
+        child;
+
+    // creating an image
+    gm(200, 100, "#ffffffff")
+        .options({imageMagick: true})
+        .transparent('#ffffffff')
+        .fill('#000000')
+        .font('Monotype-Corsiva',42)
+        .drawText(10,50,text)
+        .toBuffer('PNG',function (err, buffer) {
+            //DO SOME ERROR CHECKING HERE
+            res.set('Content-Type', 'image/png');
+            res.send(buffer);
+        });
+
 });
 
 module.exports = router;
