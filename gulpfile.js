ae4978bd17b973bfbaa7d1d4f831b0fab946ebc2
Alex Yang <alyx.yang@gmail.com>
2015-02-16 16:42:45 -0800

    added nodemon server reload on file changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,23 @@
 // gulpfile.js
 var gulp = require('gulp');
 var server = require('gulp-express');
-gulp.task('express-run', function() {
-    // Start the server at the beginning of the task
-    server.run({
-        file: './bin/www'
-    });
-});
+var nodemon = require('gulp-nodemon');
+var jshint = require('gulp-jshint');
 
-gulp.task('server', function () {
-    // Restart the server when file changes
-    gulp.watch(['app/**/*.html'], server.notify);
-    gulp.watch(['app/styles/**/*.scss'], ['styles:scss']);
-    //gulp.watch(['{.tmp,app}/styles/**/*.css'], ['styles:css', server.notify]);
-    //Event object won't pass down to gulp.watch's callback if there's more than one of them.
-    //So the correct way to use server.notify is as following:
-    gulp.watch(['{.tmp,app}/styles/**/*.css'], function(event){
-        gulp.run('styles:css');
-        server.notify(event);
-        //pipe support is added for server.notify since v0.1.5,
-        //see https://github.com/gimm/gulp-express#servernotifyevent
-    });
+gulp.task('express-run', function () {
+  nodemon({ script: 'bin/www', ext: 'html js css hjs', env: { 'NODE_ENV': 'development' }})
+    .on('change', ['lint'])
+    .on('restart', function () {
+      console.log('restarted!')
+    })
+})
 
-    gulp.watch(['app/scripts/**/*.js'], ['jshint']);
-    gulp.watch(['app/images/**/*'], server.notify);
-    gulp.watch(['app.js', 'routes/**/*.js'], ['express-run']);
-});
+gulp.task('lint', function () {
+  gulp.src('./**/*.js')
+    .pipe(jshint())
+})
 
-gulp.task('default', ['express-run','server']);
-
+gulp.task('default', ['express-run']);
 
 var karma = require('karma').server;
 /**
