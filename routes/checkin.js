0a826400c88fb3d253dea235be4c9e234dba5271
J Delaney <j.delaney.sd@gmail.com>
2015-02-17 09:03:12 -0800

    Refactored to use name rather than index. Display errors if form is not filled out yet

--- routes/checkin.js
+++ routes/checkin.js
@@ -28,29 +28,31 @@ router.get('/office/:id/checkin', function (req, res, next) {
 });
 
 //Custom Form
-function makeDropdown(options, index) {
-    var s = '<select class="form-control" name="_'+index+'" id="_'+index+'">';
+function makeDropdown(options, name, body) {
+    var s = '<select class="form-control" name="'+name+'" id="'+name+'">';
     _.each(options, function (option) {
-        s += '<option value="'+option+'">'+option+'</option> ';
+        s += '<option value="'+option+'" ' + (body[name] === option ? 'selected' : '') + '>'+option+'</option> ';
     });
     s+= '</select>';
     return s;
 }
 
 
-function makeTextfield(index) {
-    return'<input type="text" class="form-control" name="_'+index+'" id="_' + index + '">';
+function makeTextfield(name, body) {
+    return'<input type="text" class="form-control" name="'+name+'" id="' + name + '"value="' + (body[name] || '') + '">';
 }
 
-function makeFormGroup(field, index) {
+function makeFormGroup(field, index, body) {
+    var name = '_' + index;
+
     var s = '<div class="form-group">';
-    s += '<label for="_' + index + '" class="col-md-2 control-label">' + field.label + '</label>';
+    s += '<label for="' + name + '" class="col-md-2 control-label">' + field.label + '</label>';
 
     s += '<div class="col-md-10">';
     if (field.type === 'textbox') {
-        s += makeTextfield(index);
+        s += makeTextfield(name, body);
     } else if (field.type === 'dropdown') {
-        s += makeDropdown(field.options, index);
+        s += makeDropdown(field.options, name, body);
     }
     s += '</div>';
 
@@ -67,37 +69,82 @@ function makeSubmitButton() {
 
     return s;
 }
-
 /**
- * Custom Form POST route. Called when the user submits a form. If the form
- * is valid then it redirects to the next page, otherwise it will show an error.
+ *
+ *
+ * @param db The MongoDB object
+ * @param businessId The id for the business
+ * @param body The form body (used to prefill form)
+ * @param fn Callback function `fn(formHtml)`
  */
-router.post('/office/:id/customform', function (req, res, next) {
-    console.log(req.body);
-    res.send('Testing');
-});
-
-router.get('/office/:id/customform', function (req, res, next) {
-    var db = req.db;
+function makeForm(db, businessId, body, fn) {
     var businesses = db.get('businesses');
     var forms = db.get('forms');
 
     //Find the business and get their form
-    businesses.find({_id: req.params.id}, function (err, results) {
+    businesses.find({_id: businessId}, function (err, results) {
         var business = results[0]; //TODO: This assumes there is a result
         forms.find({business: business._id}, function (err, results) {
             var form = results[0]; //TODO: This also assumes there is a result
             var formHtml = '<form class="form-horizontal" method="post" action="customform" enctype="application/x-www-form-urlencoded">';
 
             _.each(form.fields, function (field, index) {
-                formHtml += makeFormGroup(field, index);
+                formHtml += makeFormGroup(field, index, body);
             });
 
             formHtml += makeSubmitButton();
 
             formHtml += '</form>';
 
-            res.render('checkin/customform', {title: 'Express', formHtml: formHtml});
+            fn(formHtml);
+        });
+    });
+}
+
+/**
+ * Custom Form POST route. Called when the user submits a form. If the form
+ * is valid then it redirects to the next page, otherwise it will show an error.
+ */
+router.post('/office/:id/customform', function (req, res, next) {
+    //Get the DB
+    var db = req.db;
+    var businesses = db.get('businesses');
+    var forms = db.get('forms');
+
+    businesses.find({_id: req.params.id}, function (err, results) {
+        var business = results[0]; //TODO: This assumes there is a result
+        forms.find({business: business._id}, function (err, results) {
+            var form = results[0]; //TODO: This assumes there is a result
+            var formResponse = {answers: []};
+
+            //Ensure that there are all the responses
+            var valid = _.every(form.fields, function (field, index) {
+                var name = '_' + index;
+                //TODO: Validation for dropdowns
+                return name in req.body && req.body[name].trim() !== '';
+            });
+
+            if (!valid) {
+                makeForm(db, req.params.id, req.body, function (formHtml) {
+                    res.render('checkin/customform', {
+                        title: 'Express',
+                        formHtml: formHtml,
+                        formError: 'You are missing required fields.'
+                    });
+                });
+            } else {
+
+            }
+        });
+    });
+});
+
+router.get('/office/:id/customform', function (req, res, next) {
+    var db = req.db;
+    makeForm(db, req.params.id, function (formHtml) {
+        res.render('checkin/customform', {
+            title: 'Express',
+            formHtml: formHtml
         });
     });
 });
