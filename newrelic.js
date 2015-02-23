9604c9fee9956dd8afef128a284860b57e638f12
Alex Yang <alyx.yang@gmail.com>
2015-02-22 17:16:07 -0800

    added new relic integration

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['RoboBetty'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '89860783ff94f8a39eec2b3f924801750805d989',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
