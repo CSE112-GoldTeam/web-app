5459f25bc036a826ef52c3dd48da972a2a674a22
balkrishnaoli <bkoli@ucsd.edu>
2015-02-23 19:55:28 -0800

    dynamic table created

--- routes/business.js
+++ routes/business.js
@@ -16,10 +16,25 @@ router.get('/', function (req, res, next) {
 router.get('/config', function (req, res, next) {
     res.render('business/config', {title: 'Express'});
 });
+//dashboard form
+router.get('/viewform', function (req, res, next) {
+    res.render('business/viewform', {title: 'Express'});
+});
 
 //Dashboard dashboard
 router.get('/dashboard', function (req, res, next) {
     res.render('business/dashboard', {title: 'Express'});
+});
+
+router.get('/api/appointments', function (req, res, next) {
+     // grab our db object from the request
+	var db = req.db;
+	var appt = db.get('appointments');
+	// query the collection
+	appt.find({ }, function(err, data) {
+	if (err) { return res.sendStatus(500, err); }
+		return res.json(200, data);
+	});
 });
 
 module.exports = router;
