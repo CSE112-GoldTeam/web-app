b7f2138f332d59fb407292d706a6199484ffa924
ktoribio <ktoribio@ucsd.edu>
2015-02-25 00:37:40 -0800

    Created nocode functionality for POST

--- routes/checkin.js
+++ routes/checkin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var _ = require('underscore');
 var router = express.Router();
+var ObjectID = require('mongodb').ObjectID;
 
 //No associated use case so far
 router.get('/office/:id/done', function (req, res, next) {
@@ -21,6 +22,8 @@ router.get('/office/:id/entercode', function (req, res, next) {
 router.get('/office/:id/nocode', function (req, res, next) {
     res.render('checkin/nocode', {title: 'Express'});
 });
+
+
 
 //Checkin Start
 router.get('/office/:id/checkin', function (req, res, next) {
@@ -117,7 +120,7 @@ router.post('/office/:id/customform', function (req, res, next) {
     var forms = db.get('forms');
 
     businesses.find({_id: req.params.id}, function (err, results) {
-        var business = results[0]; //TODO: This assumes there is a result
+				var business = results[0]; //TODO: This assumes there is a result
         forms.find({business: business._id}, function (err, results) {
             var form = results[0]; //TODO: This assumes there is a result
 
@@ -158,13 +161,135 @@ router.post('/office/:id/customform', function (req, res, next) {
 });
 
 router.get('/office/:id/customform', function (req, res, next) {
-    var db = req.db;
-    makeForm(db, req.params.id, {}, function (formHtml) {
+    var db = req.db; makeForm(db, req.params.id, {}, function (formHtml) {
         res.render('checkin/customform', {
             title: 'Express',
             formHtml: formHtml
         });
     });
+});
+
+
+/*
+ * Code that gives nocode functionality when POST
+ * detected. Will check db for appropriate info and,
+ * if found, save _id to session var and redirect to apptInfo.
+ * If input errors detected or no appointment found, error
+ * printed on screen
+ */
+router.post('/office/:id/nocode', function (req, res, next) {
+		var db = req.db;
+		var appointments = db.get('appointments');
+		var dobFormatErr = 'Please enter your Date of Birth in MM/DD/YYYY format';
+		var monthValErr = 'Please enter MM value between 01 and 12';
+		var dayValErr = 'Please enter DD value between 01 and 31';
+
+		var inputFirst = req.body.inputFirst;
+		var inputLast = req.body.inputLast;
+		var inputDOB = req.body.inputDOB;
+		var dobSubStr = req.body.inputDOB;
+		var numSlash = inputDOB.match(/\//g).length;
+		
+		var busName;
+		var aptID;
+
+		if (numSlash != 2) {
+			res.render('checkin/nocode', {
+				error: dobFormatErr, 
+				inputFirst: inputFirst,
+				inputLast: inputLast,
+				inputDOB: inputDOB
+			});
+		}
+		
+		var firstSep = dobSubStr.indexOf("/");
+		var inputMonth = dobSubStr.substring(0, firstSep);
+
+		if (inputMonth.length > 2) {
+			res.render('checkin/nocode', {
+				error: dobFormatErr,
+				inputFirst: inputFirst,
+				inputLast: inputLast,
+				inputDOB: inputDOB
+			});
+		}
+
+		dobSubStr = dobSubStr.substring(firstSep+1);
+		var secondSep = dobSubStr.indexOf("/");
+		var inputDay = dobSubStr.substring(0, secondSep);
+
+		if (inputDay.length > 2) {
+			res.render('checkin/nocode', {
+				error: dobFormatErr,
+				inputFirst: inputFirst,
+				inputLast: inputLast,
+				inputDOB: inputDOB
+			});
+		}
+
+		var inputYear = dobSubStr.substring(secondSep+1);
+		
+		if (inputYear.length != 4)
+		{
+			res.render('checkin/nocode', {
+				error: dobFormatErr,
+				inputFirst: inputFirst,
+				inputLast: inputLast,
+				inputDOB: inputDOB
+			});
+		}
+
+		var monthInt = parseInt(inputMonth);
+
+		if (monthInt < 1 || monthInt > 12)
+		{
+			res.render('checkin/nocode', {
+				error: monthValErr,
+				inputFirst: inputFirst,
+				inputLast: inputLast,
+				inputDOB: inputDOB
+			});
+		}
+		else if (monthInt < 10 && inputMonth.length == 1)
+		{
+			inputMonth = "0" + inputMonth;
+		}
+
+		var dayInt = parseInt(inputDay);
+
+		if (dayInt < 1 || dayInt > 31)
+		{
+			res.render('checkin/nocode', {
+				error: dayValErr,
+				inputFirst: inputFirst,
+				inputLast: inputLast,
+				inputDOB: inputDOB
+			});
+		}
+		else if (dayInt < 10 && inputDay.length == 1)
+		{
+			inputDay = "0" + inputDay;
+		}
+
+		inputDOB = inputMonth + "/" + inputDay + "/" + inputYear;
+	
+		appointments.find({business: req.params.id, fname: inputFirst, lname: inputLast, dob: inputDOB}, function(err, result) {
+			if (result.length == 0) {
+				res.render('checkin/nocode', {
+					error: 'No appointment found',
+					inputFirst: inputFirst,
+					inputLast: inputLast,
+					inputDOB: inputDOB
+				});
+			}
+			else {
+				var appt = result[0];
+				var apptID = appt._id;
+				req.session.apointmentId = apptID;
+				res.redirect('apptinfo');
+			}
+		});
+		
 });
 
 module.exports = router;
