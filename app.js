ae73128d53f18e608c98412d1fcfbdd078db7d54
J Delaney <j.delaney.sd@gmail.com>
2015-02-26 08:16:59 -0800

    Removed unused libs and arguments

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ var express = require('express');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var passport = require('passport');
-var router = express.Router();
 var app = express();
 
 
@@ -136,7 +134,7 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
+    app.use(function (err, req, res) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -147,7 +145,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
