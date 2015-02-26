7ad4071213730136b5411e873214098c07b5371e
J Delaney <j.delaney.sd@gmail.com>
2015-02-26 08:24:40 -0800

    Removed unused libs and vars

--- routes/signature.js
+++ routes/signature.js
@@ -1,17 +1,13 @@
 var express = require('express'),
-    fs = require('fs'),
     gm = require('gm');
 var router = express.Router();
 
-router.get('/api/signature', function (req, res, next) {
+router.get('/api/signature', function (req, res) {
     res.redirect('%20')
 })
 
-router.get('/api/signature/:text', function (req, res, next) {
+router.get('/api/signature/:text', function (req, res) {
     var text = req.params.text;
-
-    var exec = require('child_process').exec,
-        child;
 
     // creating an image
     gm(500, 100, "#ffffffff")
