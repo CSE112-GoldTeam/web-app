5864530b9c80fac15baa5848e8a54b9bdc235633
Alex Yang <alyx.yang@gmail.com>
2015-03-01 12:57:26 -0800

    added gulpfile functionality to stage to different environments

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,20 @@
 var gulp = require('gulp');
 var server = require('gulp-express');
 var child_process = require('child_process');
+var argv = require('yargs').argv;
 
 var nodemon = require('gulp-nodemon');
 var jshint = require('gulp-jshint');
 var browserSync = require('browser-sync');
 
 var mongobackup = require('mongobackup');
+var shell = require('gulp-shell');
+
+
+var exec = require('child_process').exec;
+function execute(command, callback){
+    exec(command, function(error, stdout, stderr){callback(stdout);});
+};    
 
 gulp.task('nodemon', function (cb) {
   var called = false;
@@ -96,6 +104,85 @@ gulp.task('mongorestore', function() {
 });
 
 
+// prerequisites - must have heroku command line tools installed
+//               - must be authenticated with heroku
+//               - must have git installed and be in application root directory
+//               - must be authenticated with git so that password does not have to be entered on push
+gulp.task('stage', function(){ 
+    if (argv.test == null){ 
+        execute('git symbolic-ref --short HEAD', function(br){
+            console.log('deploying current branch: ' + br);
+            return gulp.src('')
+                    .pipe(shell([
+                        'heroku git:remote -a robobetty-test1 -r test1',
+                        'git push -f test1 <%= determineBranch() %>'
+                    ], {
+                        templateData: {
+                            determineBranch: function() {
+                                var n_remote = br.trim() + ':master';
+                                return n_remote;
+                            }
+                        }
+                    }));
+        }); 
+    }
+
+    if (argv.test == 1){ 
+        execute('git symbolic-ref --short HEAD', function(br){
+            console.log('deploying current branch: ' + br);
+            return gulp.src('')
+                    .pipe(shell([
+                        'heroku git:remote -a robobetty-test1 -r test1',
+                        'git push -f test1 <%= determineBranch() %>'
+                    ], {
+                        templateData: {
+                            determineBranch: function() {
+                                var n_remote = br.trim() + ':master';
+                                return n_remote;
+                            }
+                        }
+                    }));
+        }); 
+    }
+
+    if (argv.test == 2){ 
+        execute('git symbolic-ref --short HEAD', function(br){
+            console.log('deploying current branch: ' + br);
+            return gulp.src('')
+                    .pipe(shell([
+                        'heroku git:remote -a robobetty-test2 -r test2',
+                        'git push -f test2 <%= determineBranch() %>'
+                    ], {
+                        templateData: {
+                            determineBranch: function() {
+                                var n_remote = br.trim() + ':master';
+                                return n_remote;
+                            }
+                        }
+                    }));
+        }); 
+    }
+
+
+    if (argv.test == 3){ 
+        execute('git symbolic-ref --short HEAD', function(br){
+            console.log('deploying current branch: ' + br);
+            return gulp.src('')
+                    .pipe(shell([
+                        'heroku git:remote -a robobetty-test3 -r test3',
+                        'git push -f test3 <%= determineBranch() %>'
+                    ], {
+                        templateData: {
+                            determineBranch: function() {
+                                var n_remote = br.trim() + ':master';
+                                return n_remote;
+                            }
+                        }
+                    }));
+        }); 
+    } 
+})
+
 gulp.task('default', ['browser-sync']);
 
 
@@ -109,3 +196,4 @@ gulp.task('test', function (done) {
     singleRun: true
   }, done);
 });
+
