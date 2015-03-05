206e4bc3fcd3eeb740ad0d3e0566955871aff927
J Delaney <j.delaney.sd@gmail.com>
2015-03-04 23:03:53 -0800

    Button color choosing

--- public/javascripts/theme.js
+++ public/javascripts/theme.js
@@ -1,3 +1,14 @@
+function rgb2hex(rgb) {
+    if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
+
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+        return ('0' + parseInt(x).toString(16)).slice(-2);
+    }
+
+    return '#' + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
+
 $(document).ready(function () {
     var $themeToolbar = $('#theme-toolbar');
 
@@ -21,6 +32,21 @@ $(document).ready(function () {
     //Changing BG picture
     $themeToolbar.find('.bg-image').click(function () {
         var bg = $(this).data('bg');
-        $('.customBG').css('background-image', ' url(images/bg/full/'+bg+')');
+        $('.customBG').css('background-image', ' url(images/bg/full/' + bg + ')');
+    });
+
+    //Colorpicker
+    $('#button-bg-color').spectrum({
+        color: rgb2hex($('.checkinbutton').css('background-color')),
+        move: function (color) {
+            $('.checkinbutton').css('background-color', color.toHexString());
+        }
+    });
+
+    $('#button-font-color').spectrum({
+        color: rgb2hex($('.checkinbutton').css('color')),
+        move: function (color) {
+            $('.checkinbutton').css('color', color.toHexString());
+        }
     });
 });
