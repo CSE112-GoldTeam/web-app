170ebeac548815d0f6a1f06cac6cf378651aadc3
alex gian <alexhgian@gmail.com>
2015-03-04 16:41:23 -0800

    Get Form works

--- routes/api/form/form.controller.js
+++ routes/api/form/form.controller.js
@@ -13,17 +13,21 @@ var _ = require('underscore');
 
 // Request a form
 exports.show = function(req, res) {
-
     // grab our db object from the request
-      var db = req.db;
-      var forms = db.get('forms');
 
-      // query to create entry in collection
-      forms.findById(req.params.id, function (err, doc) {
-          if(err) { return handleError(res, err); }
-          if(!doc) { return res.sendStatus(404); }// res.send is deprecated
-          return res.json(doc);
-      });
+    var db = req.db;
+    var forms = db.get('forms');
+
+    // TODO: req.business needs to work first for the business parameter to work
+    //var business = forms.id(req.business);
+    //Use this to test form id 54ecb5a6fb4974129dc20634
+    //business = forms.id("54eca979f2a2d47937757617");
+
+    forms.find({ "_id" : req.params.id }, function (err, doc) {
+        if(err) { return handleError(res, err); }
+        if(!doc) { return res.sendStatus(404); }// res.send is deprecated
+        return res.json(doc);
+    });
 };
 
 // Create a form
