16e15a805d03bda029b649a849bb47e87bc12022
balkrishnaoli <bkoli@ucsd.edu>
2015-03-09 14:40:01 -0700

    clean up the code

--- routes/webapp/business/businesssettings.js
+++ routes/webapp/business/businesssettings.js
@@ -2,15 +2,10 @@ exports.get = function (req,res) {
     var bid = '54eca953f2a2d47937757616';
     var db = req.db;
     var businesses = db.get('businesses');
-
-    var companyName;
-    var phone;
-    var email;
-
     businesses.find({_id: bid}, function (err, result) {
         var dbBusiness = result[0];
         var phone = dbBusiness.phone;
-        phone = phone.slice(0, 3) + "-" + phone.slice(3, 6) + "-" + phone.slice(6);
+        phone = phone.slice(0, 3) + '-' + phone.slice(3, 6) + '-' + phone.slice(6);
         res.render('business/businesssettings', {
             title: 'Express',
             companyName: dbBusiness.companyName,
@@ -29,34 +24,22 @@ exports.post = function (req, res) {
     var companyName = req.body.companyName;
     var email = req.body.email;
     var phone = req.body.phone;
-    console.log("req.body.phone-------------->>...:  "+ req.body.phone); 
-    // var oldPassword = req.body.oldPassword;
-    // var newPassword = req.body.newPassword;
-    // var confirmPassword = req.body.confirmPassword;
     businesses.find({_id: bid}, function (err, result) {
          var dbBusiness = result[0];
          
 
         if (companyName === '' || email === '' || phone === '') {
             phone = dbBusiness.phone;
-            phone = phone.slice(0, 3) + "-" + phone.slice(3, 6) + "-" + phone.slice(6);
+            phone = phone.slice(0, 3) + '-' + phone.slice(3, 6) + '-' + phone.slice(6);
             res.render('business/businesssettings', {
-                error: "You must fill in all fields.",
+                error: 'You must fill in all fields.',
                 companyName: dbBusiness.companyName,
                 email: email,
                 phone: phone
             });
         }
-        // else if (oldpassword !== dbpassword || oldPassword === null || newPassword !== confirmPassword ||
-        //     newPassword === null || confirmPassword === null) {
-        //     //Show an error to the user
-        //     res.render('business/businesssettings', {
-        //         error: 'Your passwords must be nonblank and must match.'
-        //     });
-        // }
          else {
-            phone = phone.replace(/-/g, "");
-            console.log("this is getting fraustrated:  "+ phone);
+            phone = phone.replace(/-/g, '');
             businesses.findAndModify({_id:bid}, {
                 $set :{
                     companyName: companyName,
@@ -64,12 +47,12 @@ exports.post = function (req, res) {
                     phone: phone
                 }
             });
-            phone = phone.slice(0, 3) + "-" + phone.slice(3, 6) + "-" + phone.slice(6);
+            phone = phone.slice(0, 3) + '-' + phone.slice(3, 6) + '-' + phone.slice(6);
             res.render('business/businesssettings', {
-                companyName: dbBusiness.companyName,
+                companyName: companyName,
                 email: email,
                 phone: phone,
-                edited: "change successfully done."
+                edited: 'change successfully done.'
             });
         }
     });
