2915b41fdbce096ef7ea3b9a8f5fd5a9ac5ae66e
Giovanni Wong <deargiovanni89@gmail.com>
2015-03-09 10:47:24 -0700

    refactored code, now texting

--- routes/webapp/business/addemployees.js
+++ routes/webapp/business/addemployees.js
@@ -1,74 +1,95 @@
 var baby = require('babyparse');
+var async = require('async');
+var crypto = require('crypto');
 var sendgrid  = require('sendgrid')('robobetty', 'NoKcE0FGE4bd');
 
 
 exports.get = function (req,res){
     var db =  req.db;
-    var csvEmployees = db.get('csvEmployees');
-    csvEmployees.find({registrationToken: {$exists: false}},function (err,results){
+    var csvEmployees = db.get('employees');
+    var employee;
+    var notemployee;
+    
 
-        if (err) { return res.sendStatus(500, err); }
-        if(!results) { return res.send(404,'User not found');}
+    async.parallel({
+        employee: function(cb){
+            csvEmployees.find({registrationToken: {$exists: false}},function (err,results){
 
-        employee = results;
+                if (err) { return res.sendStatus(500, err); }
+                if(!results) { return res.send(404,'User not found');}
+                 
+                    employeee = results;
+                   cb();
+            
+            });
+        },
+        nonemployee: function(cb){
+            csvEmployees.find({registrationToken: {$exists: true}}, function (err,results){
+
+
+                if (err) { return res.sendStatus(500, err); }
+                if(!results) { return res.send(404,'User not found');}
+                  
+                     notemployee = results;
+                     cb();
+            });
+        }
+    },
+
+    function(err,results){
+
+        if(err){
+            throw err;
+        }
+        res.render('business/addemployees',{title: 'Express',notsigned: notemployee, signed: employeee});
 
     });
-
-    csvEmployees.find({registrationToken: {$exists: true}}, function (err,results){
-
-
-        if (err) { return res.sendStatus(500, err); }
-        if(!results) { return res.send(404,'User not found');}
-
-        notemployee = results;
-
-    });
-
-
-
-    res.render('business/addemployees',{title: 'Express',notsigned: notemployee, signed: employee});
 
 };
 
 exports.post = function (req,res){
 
 
-    parsed = baby.parse(req.body.csvEmployees);
-    rows = parsed.data;
+    var parsed = baby.parse(req.body.csvEmployees);
+    var rows = parsed.data;
 
 
+    var number = 0;
+    rows.forEach(function (d){
+        number += 1;
+    });
 
+    var db =  req.db;
+    var csvEmployees = db.get('employees');
 
-    username = rows[0][0];
-    email = rows[0][1];
+for(var i = 0; i < number; i++){
+   var username = rows[i][0];
+   var email = rows[i][1];
 
     var token = randomToken();
+    csvEmployees.insert({
+    name: username,
+    email: email,
+    registrationToken : token,
+    });
 
 
-    sendgrid.send({
+      sendgrid.send({
         to: email,
         from: 'test@localhost',
         subject: 'Employee Signup',
         text: 'Hello ' + username + ',\n\n' + 'Please click on the following link, or paste this into your browser to complete sign-up the process: \n\n' +
-        'http://robobetty/register/?token=' + token
-    }, function (err, json){
+        'http://localhost:3000/employeeregister?token=' + token 
+    }, function (err){
         if (err) {
             return console.error(err);
         }
-        var db =  req.db;
-        var csvEmployees = db.get('csvEmployees');
-        csvEmployees.insert({
-            name: username,
-            email: email,
-            registrationToken : token,
-        },{
-            w: 1
-        }, function (err){
-            if (err) {
-                return console.error(err);
-            }
-            res.redirect('/addemployees');
-        })
-    });
+      });
+}
 
 };
+
+
+function randomToken() {
+    return crypto.randomBytes(24).toString('hex');
+}
