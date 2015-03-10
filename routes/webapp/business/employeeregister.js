2180184cfe8053ccbf46cc75c8b6745d3813e28a
Giovanni Wong <deargiovanni89@gmail.com>
2015-03-09 20:35:48 -0700

    added employeeregister, but login and sign up still doesnt work

--- routes/webapp/business/employeeregister.js
+++ routes/webapp/business/employeeregister.js
@@ -1,11 +1,16 @@
+var passport = null;
+exports.init = function (passportIn) {
+    passport = passportIn;
+};
+
 exports.get = function(req,res){
     res.render('business/registeremployees');
 };
 
 
-exports.post = function (req){
-    var db =req.db;
-    var employee = db.get('csvEmployees');
-    employee.update({'token': req.query.token}, function (err,results){
+exports.post = function (){
+     return passport.authenticate('local-signup-employee',{
+        successRedirect : '/config', // redirect to the secure profile section
+        failureRedirect : '/register' // redirect back to the signup page if there is an error
     });
 };
