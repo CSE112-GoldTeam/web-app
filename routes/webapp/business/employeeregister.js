f41820f65b14d9b7f7fbcfa29206ffe7f789ee76
Giovanni Wong <deargiovanni89@gmail.com>
2015-03-10 01:21:37 -0700

    refactored employeeregister to index.js

--- routes/webapp/business/employeeregister.js
+++ routes/webapp/business/employeeregister.js
@@ -1,16 +1,8 @@
-var passport = null;
-exports.init = function (passportIn) {
-    passport = passportIn;
-};
+
 
 exports.get = function(req,res){
     res.render('business/registeremployees');
 };
 
 
-exports.post = function (){
-     return passport.authenticate('local-signup-employee',{
-        successRedirect : '/config', // redirect to the secure profile section
-        failureRedirect : '/register' // redirect back to the signup page if there is an error
-    });
-};
+
