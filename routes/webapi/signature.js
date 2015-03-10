b24e3ed33048659e2b8dff644d687bbe6cdfe30f
Young Shiau <youngshiau@gmail.com>
2015-03-09 18:13:46 -0700

    Documented routes/webapi/signature.js

--- routes/webapi/signature.js
+++ routes/webapi/signature.js
@@ -1,9 +1,23 @@
+/**
+ * GraphicsMagick plugin
+ */
 var gm = require('gm');
 
 exports.getDefault = function (req, res) {
     res.redirect('%20');
 };
 
+/**
+ * Creates an image by converting the a String (the user's input) into an
+ * image. The image is created by drawing the user's input as text with a
+ * cursive font on a transparent background, and serves as an electronic 
+ * signature.
+ *
+ * @param req
+ * @param res
+ * @returns {Object} A Buffer object containing an image of the user's 
+ * 'signature' in PNG format.
+ */
 exports.get = function (req, res) {
     var text = req.params.text;
 
