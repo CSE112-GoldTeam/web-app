f13a1d18efccca8be04ec17098f842e70ad1e1e3
J Delaney <j.delaney.sd@gmail.com>
2015-03-11 22:29:04 -0700

    Dynamic theming for theme page

--- routes/webapp/business/theming.js
+++ routes/webapp/business/theming.js
@@ -1,4 +1,21 @@
-exports.get = function (req, res) {
+var style = require('./../../../lib/style.js');
+
+exports.get = function (req, res, next) {
     var bid = req.user.Business[0]._id;
-    res.render('business/theme');
+
+    req.db.get('businesses').findById(bid, function (err, business) {
+        if (err) {
+            return next(err);
+        }
+
+        res.render('business/theme', {
+            companyName: business.companyName,
+            bg: business.style.bg,
+            logo: business.logo,
+            buttonBg: style.rgbObjectToCSS(business.style.buttonBg),
+            buttonText: style.rgbObjectToCSS(business.style.buttonText),
+            containerText: style.rgbObjectToCSS(business.style.containerText),
+            containerBg: style.rgbObjectToCSS(business.style.containerBg)
+        });
+    });
 };
