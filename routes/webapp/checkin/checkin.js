5e3cae0560d773d1f267bddd99858aca529b32f8
J Delaney <j.delaney.sd@gmail.com>
2015-03-11 21:12:32 -0700

    Finished making checkin page dynamic

--- routes/webapp/checkin/checkin.js
+++ routes/webapp/checkin/checkin.js
@@ -1,12 +1,21 @@
+var style = require('./../../../lib/style.js');
+
 exports.get = function (req, res, next) {
-    //Find the current business
     req.db.get('businesses').findById(req.params.id, function (err, business) {
         if (err) {
             return next(err);
         }
 
+
+
         res.render('checkin/checkin', {
-            bg: business.style.bg
+            companyName: business.companyName,
+            bg: business.style.bg,
+            logo: business.logo,
+            buttonBg: style.rgbObjectToCSS(business.style.buttonBg),
+            buttonText: style.rgbObjectToCSS(business.style.buttonText),
+            containerText: style.rgbObjectToCSS(business.style.containerText),
+            containerBg: style.rgbObjectToCSS(business.style.containerBg)
         });
     });
 };
