2ab544e0c8fd431ec329a4a6680d3a87ba0ee8d5
Alex Gian <alexhgian@gmail.com>
2015-03-11 19:14:15 -0700

    Update [timestamp] --skip-ci

--- api_data.js
+++ api_data.js
@@ -1,10 +1,15 @@
 define({ "api": [
   {
     "type": "get",
-    "url": "/api/m/appointment?fname=John&lname=\"Doe\"&dob=\"05/13/1965\"",
+    "url": "/api/m/appointment",
     "title": "",
-    "name": "GetAppointmentConfirm",
+    "name": "confirm",
     "group": "Appointment",
+    "permission": [
+      {
+        "name": "admin"
+      }
+    ],
     "parameter": {
       "fields": {
         "Parameter": [
@@ -21,10 +26,24 @@ define({ "api": [
             "optional": false,
             "field": "lname",
             "description": "<p>Lastname of the User.</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "dob",
+            "description": "<p>Date of birth of the User.</p> "
           }
         ]
       }
     },
+    "examples": [
+      {
+        "title": "Example usage:",
+        "content": "curl -i http://localhost/api/m/appointment?fname=John&lname=\"Doe\"&dob=\"05/13/1965",
+        "type": "json"
+      }
+    ],
     "success": {
       "fields": {
         "Success 200": [
@@ -37,6 +56,31 @@ define({ "api": [
           }
         ]
       }
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "NoAccessRight",
+            "description": "<p>Only authenticated Admins can access the data.</p> "
+          },
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "ApptNotFound",
+            "description": "<p>The <code>id</code> of the User was not found.</p> "
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Response (example):",
+          "content": "HTTP/1.1 401 Not Authenticated\n{\n  \"error\": \"NoAccessRight\"\n}",
+          "type": "json"
+        }
+      ]
     },
     "version": "0.0.0",
     "filename": "routes/api/appointment/index.js",
