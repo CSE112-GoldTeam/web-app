be7a51d795a82ee5da7227b578dbd574b1157291
alex gian <alexhgian@gmail.com>
2015-03-12 02:44:45 -0700

    More Doc

--- routes/api/appointment/index.js
+++ routes/api/appointment/index.js
@@ -20,20 +20,34 @@ var auth = require('../../../lib/auth');
  * @apiExample Example usage:
  * curl -i http://localhost/api/m/appointment?fname=John&lname="Doe"&dob="05/13/1965
  *
- * @apiSuccess {String} fname Firstname of the User.
+ * @apiSuccess { ObjectID } business Business ID
+ * @apiSuccess { ObjectID } employee Employee ID
+ * @apiSuccess { Date } date Date of the appointment
+ * @apiSuccess { String } lname Last name
+ * @apiSuccess { String } fname First name
+ * @apiSuccess { String } dob Date of birth MM/DD/YYYY
+ * @apiSuccess { String } email Email
+ * @apiSuccess { String } state See db scheme for possible states
  *
- * @apiSuccessExample Request (example):
+ * @apiSuccessExample {json} Success-Response (example):
  * HTTP/1.1 200 OK
- * {
- *  "appointment": [
- *     "appointmentObject"
- *  ]
- * }
+ * [
+ *  {
+ *  	"business" : ObjectId("54eca953f2a2d47937757616"),
+ *  	"employee" : ObjectId("54ecaa24fb4974129dc2050c"),
+ *  	"date" : ISODate("2015-02-26T21:00:00Z"),
+ *  	"fname" : "Emily",
+ *  	"lname" : "Lee",
+ *  	"dob" : "03/25/1968",
+ *  	"email" : "Emily.Lee@example.com",
+ *  	"state" : "scheduled"
+ *  }
+ * ]
  *
  * @apiError NoAccessRight Only authenticated Admins can access the data.
  * @apiError ApptNotFound  The <code>id</code> of the User was not found.
  *
- * @apiErrorExample Response (example):
+ * @apiErrorExample Error (example):
  *     HTTP/1.1 401 Not Authenticated
  *     {
  *       "error": "NoAccessRight"
