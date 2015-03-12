a8e2b1b623b951a282d08a553a2c9a6c226586d8
Alex Gian <alexhgian@gmail.com>
2015-03-11 22:32:00 -0700

    Update API Doc --skip-ci

--- api_project.js
+++ api_project.js
@@ -1,12 +1,12 @@
 define({
   "name": "robobetty",
-  "version": "0.0.0",
+  "version": "0.6.0",
   "description": "apiDoc for Robobetty",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-03-12T04:21:12.449Z",
+    "time": "2015-03-12T05:31:59.998Z",
     "url": "http://apidocjs.com",
     "version": "0.12.1"
   }
