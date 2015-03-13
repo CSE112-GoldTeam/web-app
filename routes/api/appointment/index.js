9ad741d6b847f8f197865aefba0c835968fc4fcc
alex gian <alexhgian@gmail.com>
2015-03-13 16:43:20 -0700

    More comments and apidoc header added

--- routes/api/appointment/index.js
+++ routes/api/appointment/index.js
@@ -9,65 +9,20 @@ var auth = require('../../../lib/auth');
 router.get('/index', controller.index);
 
 /**
- * @api {get} /m/appointment Confirm an Appointment
- * @apiName confirm
- * @apiGroup Appointment
- * @apiPermission admin
- *
- * @apiParam {String} fname Firstname of the User.
- * @apiParam {String} lname Lastname of the User.
- * @apiParam {String} dob Date of birth of the User.
- *
- * @apiExample Example usage:
- * curl -H
- -i http://localhost:3000/api/m/appointment?fname='Frodo'&lname='St. John'&dob='02/17/1956'
- *
-* @apiExample Temporary No Auth Example usage:
-* curl -i http://localhost:3000/api/m/appointment?fname=Frodo\
-&lname=St. John&dob=02/17/1956&bid=5500a18ac0a954cae1bbf238
+* @api {get} /m/appointment Confirm an Appointment
+* @apiName confirm
+* @apiGroup Appointment
+* @apiPermission admin
 *
- * @apiSuccess { ObjectID } business Business ID
- * @apiSuccess { ObjectID } employee Employee ID
- * @apiSuccess { Date } date Date of the appointment
- * @apiSuccess { String } lname Last name
- * @apiSuccess { String } fname First name
- * @apiSuccess { String } dob Date of birth MM/DD/YYYY
- * @apiSuccess { String } email Email
- * @apiSuccess { String } state See db scheme for possible states
- *
- * @apiSuccessExample {json} Success-Response (example):
- * HTTP/1.1 200 OK
- * [
- *  {
- *  	"business" : ObjectId("54eca953f2a2d47937757616"),
- *  	"employee" : ObjectId("54ecaa24fb4974129dc2050c"),
- *  	"date" : ISODate("2015-02-26T21:00:00Z"),
- *  	"fname" : "Emily",
- *  	"lname" : "Lee",
- *  	"dob" : "03/25/1968",
- *  	"email" : "Emily.Lee@example.com",
- *  	"state" : "scheduled"
- *  }
- * ]
- *
- * @apiErrorExample Error (example):
- *     HTTP/1.1 401 Not Authenticated
- */
-router.get('/', auth.isAuthenticated, controller.confirm);
-
-/**
- * @api {get} /m/appointment/:id Get Appointment Info
- * @apiVersion 0.6.0
- * @apiName retrieve
- * @apiGroup Appointment
- * @apiPermission admin
- *
- * @apiParam {String} id Appointment id
- *
- *
- * @apiExample Example usage:
- * curl -i http://localhost/api/m/appointment/123456789
- *
+* @apiParam {String} fname Firstname of the User.
+* @apiParam {String} lname Lastname of the User.
+* @apiParam {String} dob Date of birth of the User.
+*
+* @apiExample Example usage:
+* curl -i http://localhost:3000/api/m/appointment?fname='Frodo'&lname='St. John'&dob='02/17/1956' \
+-H "Authentication": "Token 550286024ae861626c9235f4"
+*
+*
 * @apiSuccess { ObjectID } business Business ID
 * @apiSuccess { ObjectID } employee Employee ID
 * @apiSuccess { Date } date Date of the appointment
@@ -91,34 +46,98 @@ router.get('/', auth.isAuthenticated, controller.confirm);
 *  	"state" : "scheduled"
 *  }
 * ]
-* @apiErrorExample Error (example):
+*
+* @apiErrorExample Error-Response (example):
 *     HTTP/1.1 401 Not Authenticated
- */
+*/
+router.get('/', auth.isAuthenticated, controller.confirm);
+
+/**
+* @api {get} /m/appointment/:id Get Appointment Info
+* @apiVersion 0.6.0
+* @apiName retrieve
+* @apiGroup Appointment
+* @apiPermission admin
+*
+* @apiParam {String} id Appointment id
+*
+*
+* @apiExample Example usage:
+* curl -i http://localhost/api/m/appointment/123456789 \
+-H "Authentication": "Token 550286024ae861626c9235f4"
+*
+* @apiSuccess { ObjectID } business Business ID
+* @apiSuccess { ObjectID } employee Employee ID
+* @apiSuccess { Date } date Date of the appointment
+* @apiSuccess { String } lname Last name
+* @apiSuccess { String } fname First name
+* @apiSuccess { String } dob Date of birth MM/DD/YYYY
+* @apiSuccess { String } email Email
+* @apiSuccess { String } state See db scheme for possible states
+*
+* @apiSuccessExample {json} Success-Response (example):
+* HTTP/1.1 200 OK
+* [
+*  {
+*  	"business" : ObjectId("54eca953f2a2d47937757616"),
+*  	"employee" : ObjectId("54ecaa24fb4974129dc2050c"),
+*  	"date" : ISODate("2015-02-26T21:00:00Z"),
+*  	"fname" : "Emily",
+*  	"lname" : "Lee",
+*  	"dob" : "03/25/1968",
+*  	"email" : "Emily.Lee@example.com",
+*  	"state" : "scheduled"
+*  }
+* ]
+*
+* @apiErrorExample Error-Response (example):
+*     HTTP/1.1 401 Not Authenticated
+*/
 router.get('/:id', auth.isAuthenticated, controller.retrieve);
 
 /**
- * @api {put} /m/appointment/:id/state/next Transition to Next State
- * @apiName controller.nextState
- * @apiGroup Appointment
- * @apiPermission admin
- *
- * @apiParam {String} id Appointment id
- * @apiSuccessExample Success-Response (example):
- * HTTP/1.1 200 OK
- */
+* @api {put} /m/appointment/:id/state/next Transition to Next State
+* @apiName controller.nextState
+* @apiGroup Appointment
+* @apiPermission admin
+*
+* @apiParam {String} id Appointment id
+*
+* @apiExample Example usage:
+curl -X PUT -i http://localhost:3000/api/m/appointment/12345613/state/next \
+-H "Authentication": "Token 550286024ae861626c9235f4" \
+*
+*
+*
+* @apiSuccessExample Success-Response (example):
+* HTTP/1.1 200 OK
+*
+* @apiErrorExample Error-Response (example):
+*     HTTP/1.1 401 Not Authenticated
+*/
 router.put('/:id/state/next', auth.isAuthenticated, controller.nextState);
 
 /**
- * @api {put} /m/appointment/:id/state Set a Specific State
- * @apiName controller.updateState
- * @apiGroup Appointment
- * @apiPermission admin
- *
- * @apiParam {String} id Appointment id
- *
- * @apiSuccessExample Success-Response (example):
- * HTTP/1.1 200 OK
- */
+* @api {put} /m/appointment/:id/state Set a Specific State
+* @apiName controller.updateState
+* @apiGroup Appointment
+* @apiPermission admin
+*
+* @apiParam {String} id Appointment id
+* @apiParam {String} state State to set. See database schema for see possible states.
+*
+* @apiExample Example usage:
+curl -X PUT -i http://localhost:3000/api/m/appointment/12345613/state \
+-H "Authentication": "Token 550286024ae861626c9235f4" \
+-H "Content-Type: application/json" \
+-d '{"state":"roomed"}'
+*
+* @apiSuccessExample Success-Response (example):
+* HTTP/1.1 200 OK
+*
+* @apiErrorExample Error-Response (example):
+*     HTTP/1.1 401 Not Authenticated
+*/
 router.put('/:id/state', auth.isAuthenticated, controller.updateState);
 
 
--- routes/api/form/index.js
+++ routes/api/form/index.js
@@ -7,92 +7,118 @@ var router = express.Router();
 var auth = require('../../../lib/auth');
 
 /**
- * @api {get} /m/form/:id Get a form
- * @apiName controller.show
- * @apiGroup Form
- * @apiPermission Admin
- *
- * @apiExample Example usage:
- * curl -i http://localhost/api/m/form/:id
- *
- * @apiParam {String} id The id for a form.
- *
- * @apiSuccessExample {json} Success-Response (example):
- *      HTTP/1.1 200 OK
- *      {
- *          business: 1293182391203,
- *          fields: [ {
- *              type: "textfield",
- *              label: "Name"
- *      },
- *      {
- *              type: "dropdown",
- *              label: "Gender",
- *              options: ["Male", "Female"]
- *      },
- *      {
- *              type: "textfield",
- *              label: "Email"
- *      },
- *      {
- *              type: "dropdown",
- *              label: "Favorite Color",
- *              options: ["Blue", "Yellow", "Green", "Pink"]
- *      }]
- *      }
- *
- * @apiError NoAccessRight Only authenticated Admins can access the data.
- * @apiError ApptNotFound  The <code>id</code> of the User was not found.
- *
- * @apiErrorExample Response (example):
- *     HTTP/1.1 401 Not Authenticated
- *     {
- *       "error": "NoAccessRight"
- *     }
- */
+* @api {get} /m/form/:id Get a form
+* @apiName controller.show
+* @apiGroup Form
+* @apiPermission Admin
+*
+* @apiExample Example usage:
+* curl -i http://localhost/api/m/form/:id
+*
+* @apiParam {String} id The id for a form.
+*
+* @apiSuccessExample {json} Success-Response (example):
+*      HTTP/1.1 200 OK
+*      {
+*          business: "948943204910",
+*          fields: [ {
+*              type: "textfield",
+*              label: "Name"
+*      },
+*      {
+*              type: "dropdown",
+*              label: "Gender",
+*              options: ["Male", "Female"]
+*      },
+*      {
+*              type: "textfield",
+*              label: "Email"
+*      },
+*      {
+*              type: "dropdown",
+*              label: "Favorite Color",
+*              options: ["Blue", "Yellow", "Green", "Pink"]
+*      }]
+*      }
+*
+* @apiError NoAccessRight Only authenticated Admins can access the data.
+* @apiError ApptNotFound  The <code>id</code> of the User was not found.
+*
+* @apiErrorExample Response (example):
+*     HTTP/1.1 401 Not Authenticated
+*     {
+*       "error": "NoAccessRight"
+*     }
+*/
 router.get('/:id', auth.isAuthenticated, controller.show);
 
 
 /**
- * @api {post} /m/form/:id Create a form
- * @apiName controller.createForm
- * @apiGroup Form
- * @apiPermission Admin
- *
- * @apiParam {String} id The id for a form.
- * @apiParam {String} id The id for a form.
- *
- * @apiParamExample {json} Request-Example:
- * {
- *  "answers": [
- *    {
- *      "label": "Name",
- *      "response": "John Doe"
- *    },
- *    {
- *      "label": "Gender",
- *      "response": "Female"
- *    },
- *    {
- *      "label": "Email",
- *      "response": "john.doe@example.com"
- *    },
- *    {
- *      "label": "Favorite Color",
- *      "response": "Blue"
- *    }
- *  ]
- * }
- */
+* @api {post} /m/form/:id Create a form
+* @apiName controller.createForm
+* @apiGroup Form
+* @apiPermission Admin
+*
+* @apiParam {String} id The id for a form.
+* @apiParam {String} id The id for a form.
+*
+* @apiParamExample {json} Request-Example:
+*{
+*    business: "948943204910",
+*    fields: [
+*            {
+*                    type: "textfield",
+*                    label: "Name"
+*            },
+*            {
+*                    type: "dropdown",
+*                    label: "Gender",
+*                    options: ["Male", "Female"]
+*            },
+*            {
+*                    type: "textfield",
+*                    label: "Email"
+*            },
+*            {
+*                    type: "dropdown",
+*                    label: "Favorite Color",
+*                    options: ["Blue", "Yellow", "Green", "Pink"]
+*            }
+*    ]
+*}
+*/
 router.post('/', controller.createForm);
 
 /**
- * @api {post} /m/form/ Create a form response
- * @apiName controller.createResponse
- * @apiGroup Form
- * @apiPermission Admin
- *
- */
+* @api {post} /m/form/ Create a form response
+* @apiName controller.createResponse
+* @apiGroup Form
+* @apiPermission Admin
+*
+* @apiParamExample {json} Request-Example:
+* {
+*  "business" : "1231254124",
+*  "appointment" : "12321312312",
+*  "answers": [
+*    {
+*      "label": "Name",
+*      "response": "John Doe"
+*    },
+*    {
+*      "label": "Gender",
+*      "response": "Female"
+*    },
+*    {
+*      "label": "Email",
+*      "response": "john.doe@example.com"
+*    },
+*    {
+*      "label": "Favorite Color",
+*      "response": "Blue"
+*    }
+*  ]
+* }
+*/
 router.post('/formResponse', auth.isAuthenticated, controller.createResponse);
 
 module.exports = router;
