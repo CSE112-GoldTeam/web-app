b5f97628d8724dd9da9f82b59398227f4a8cca0b
alex gian <alexhgian@gmail.com>
2015-03-13 00:21:45 -0700

    Auth working and more comments

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -35,26 +35,22 @@ function decodeAuthString(authString) {
  * @apiGroup Authentication
  * @apiPermission Admin
  *
- * @apiHeader {String} api_token The api token
+ * @apiHeader {String} api_token returned from api/auth
+
  * @apiHeaderExample {json} Header-Example:
  *     {
- *       "Authentication": "Token WW9sbzp5b2xv"
+ *       "Authentication": "Token 550286024ae861626c9235f4"
  *     }
  *
- * @apiExample Example usage:
- * curl -i http://localhost/api/authTest -H "Authorization: Token WW9sbzp5b2xv"
+* @apiExample Example usage:
+curl -X POST -i http://localhost:3000/api/authTest \
+-H "Authorization: Token 550286024ae861626c9235f4"
  *
  * @apiSuccessExample Request (example):
  * HTTP/1.1 200 OK
  *
- * @apiError NoAccessRight Only authenticated Admins can access the data.
- * @apiError ApptNotFound  The <code>id</code> of the User was not found.
- *
- * @apiErrorExample Response (example):
+ * @apiErrorExample Error Response (example):
  *     HTTP/1.1 401 Not Authenticated
- *     {
- *       "error": "NoAccessRight"
- *     }
  */
 router.post('/api/authTest', function (req, res) {
     auth.isValidToken(req.db, req.headers.authorization, function (result) {
@@ -74,35 +70,23 @@ router.post('/api/authTest', function (req, res) {
  *
  * @apiHeader {String} Authentication The api_token created from a base64 encoded
  string with email appended to password semicolon. ex. "email:password"
- * @apiHeaderExample Header-Example:
- *     {
- *       "Authentication": "Token WW9sbzp5b2xv"
- *     }
  *
  * @apiParam {String} name Name of the employee to be authenticated
  *
  * @apiExample Example usage:
- * curl -i http://localhost:3000/api/auth
- -H "Authorization: Basic bm9ydGh3b29kLmRlbnRhbEBnbWFpbC5jb206cGFzc3dvcmQ="
- -H "Content-Type: application/json"
+ curl -X POST -i http://localhost:3000/api/auth \
+ -H "Authorization: Basic bm9ydGh3b29kLmRlbnRhbEBnbWFpbC5jb206cGFzc3dvcmQ=" \
+ -H "Content-Type: application/json" \
  -d '{"name":"Frodo"}'
 
- * @apiExample Example (copy-paste) usage:
- * curl -i http://localhost:3000/api/auth -H "Authorization: Basic bm9ydGh3b29kLmRlbnRhbEBnbWFpbC5jb206cGFzc3dvcmQ=" -H "Content-Type: application/json" -d '{"name":"Frodo"}'
  * @apiSuccessExample {json} Response (example):
  * HTTP/1.1 200 OK
  *     {
  *       "api_token": "WW9sbzp5b2xv"
  *     }
  *
- * @apiError NoAccessRight Only authenticated Admins can access the data.
- * @apiError ApptNotFound  The <code>id</code> of the User was not found.
- *
- * @apiErrorExample Response (example):
+ * @apiErrorExample Error Response (example):
  *     HTTP/1.1 401 Not Authenticated
- *     {
- *       "error": "NoAccessRight"
- *     }
  */
 router.post('/api/auth', function (req, res, next) {
     if (!req.headers.authorization) {
@@ -126,7 +110,10 @@ router.post('/api/auth', function (req, res, next) {
             if (name === '' || !name) {
                 return res.send(400, 'Name field required');
             }
-            console.log(result.business);
+            if (!result.business) {
+                return res.send(404, 'Business not found. Name or password maybe wrong.');
+            }
+
             var mobileTokens = req.db.get('mobileTokens');
             mobileTokens.insert({
                 business: result.business._id,
