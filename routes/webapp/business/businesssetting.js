b8c52048644175048ad98f00c8e28cc61c6aa324
balkrishnaoli <bkoli@ucsd.edu>
2015-03-14 18:06:15 -0700

    solving the bug for password setting

--- routes/webapp/business/businesssetting.js
+++ routes/webapp/business/businesssetting.js
@@ -28,23 +28,21 @@ exports.post = function (req, res) {
     var oldPassword = req.body.oldPassword;
     var newPassword = req.body.newPassword;
     var confirmPassword = req.body.confirmPassword;
-     console.log("oldPassword:       "+oldPassword);
-     console.log("newPassword:       "+newPassword);
-     console.log("confirmPassword:       "+confirmPassword);
-
-
 
     businesses.find({_id: bid}, function (err, result) {
          var dbBusiness = result[0];
-            
+         var dbPassword = result[0].password;
+         phone = dbBusiness.phone;
+         //checks and makes sure to only perform a name, phone email setting
+        if(!oldPassword && !newPassword && !confirmPassword && phone && email && companyName) {
+            console.log("i am inside name setting yo ---___>>>+++++=====");
             if (companyName === '' || email === '' || phone === '') {
-                phone = dbBusiness.phone;
                 phone = phone.slice(0, 3) + '-' + phone.slice(3, 6) + '-' + phone.slice(6);
                 res.render('business/businesssetting', {
                     error: 'You must fill in all fields.',
                     companyName: dbBusiness.companyName,
                     email: dbBusiness.email,
-                    phone: dbBusiness.phone
+                    phone: phone
                 });
             }
              else {
@@ -58,38 +56,55 @@ exports.post = function (req, res) {
                 });
                 phone = phone.slice(0, 3) + '-' + phone.slice(3, 6) + '-' + phone.slice(6);
                 res.render('business/businesssetting', {
-                    companyName: companyName,
-                    email: email,
+                    companyName: dbBusiness.companyName,
+                    email: dbBusiness.email,
                     phone: phone,
                     edited: 'change successfully done.'
                 });
             }
+        }// end of undefined password if statement
+
+        //performs only if password submit is pressed
+        else if (!phone && !companyName && !email) {
+            console.log("Inside else ifXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            if (oldPassword && newPassword && confirmPassword){
+                //var boolPsw = auth.validPassword(dbPassword, oldPassword);
+                console.log("confirmPassword-------->>>> :"+confirmPassword);
+                if (/*boolPsw && */ newPassword === confirmPassword) {
+                    //console.log("hashPassword-------->>>> : "+newPassword+'-->'+auth.hashPassword(newPassword));
+                   // newPassword = auth.hashPassword(newPassword);
+                    businesses.findAndModify({_id:bid}, {
+                        $set :{
+                            password: newPassword,
+                        }
+                    });
+                    phone = phone.slice(0, 3) + '-' + phone.slice(3, 6) + '-' + phone.slice(6);
+                    res.render('business/businesssetting', {
+                        companyName: dbBusiness.companyName,
+                        email: dbBusiness.email,
+                        phone: phone,
+                        edited: 'password successfully changed.'
+                    });
+                }
+                
+            }
+            else {
+                phone = dbBusiness.phone;
+                phone = phone.slice(0, 3) + '-' + phone.slice(3, 6) + '-' + phone.slice(6);
+                res.render('business/businesssetting', {
+                    error: 'You must fill in all fields of password.',
+                    companyName: dbBusiness.companyName,
+                    email: dbBusiness.email,
+                    phone: phone
+                });
+            }
+
+        }//end of elseif statement
+        //$2a$08$PSCbjEeE2ymJTfY3buscq.ozt3J8iHcIaHQ4yHKzPeETYvcywJX
+        //$2a$08$PSCbjEeE2ymJTfY3buscq.ozt3J8iHcIaHQ4yHKzPeETYvcywJX3S"
+            
 
     });
 
     
 }; 
-
-
-
- //    var dbPassword = result[0].password;
-         //    var boolPsw = auth.validPassword(dbPassword, oldPassword);
-         //    if (boolPsw && newPassword === confirmPassword) {
-         //        newPassword = auth.hashPassword(newPassword);
-         //        businesses.findAndModify({_id:bid}, {
-         //            $set :{
-         //                password: newPassword
-         //            }
-         //        });
-         //        phone = phone.slice(0, 3) + '-' + phone.slice(3, 6) + '-' + phone.slice(6);
-         //        res.render('business/businesssetting', {
-         //            companyName: companyName,
-         //            email: email,
-         //            phone: phone,
-         //            edited: 'change successfully done.'
-         //        });
-         //    }
-         //    else {
-
-
-
