e70d20b7461c17950f61a8eb519ef8f551a139c3
ktoribio <ktoribio@ucsd.edu>
2015-03-15 14:47:58 -0700

    Finished set disclosure page, added alerts

--- routes/webapp/business/setdisclosure.js
+++ routes/webapp/business/setdisclosure.js
@@ -6,7 +6,6 @@ exports.get = function(req, res) {
 	var business = db.get('businesses');
 	business.find({_id: bid}, function (err, result) {
 		var dbBusiness = result[0];
-		//console.log(result);
 		var disclosure = dbBusiness.disclosure;
 
 		disclosure = disclosure.slice(3);
@@ -24,20 +23,29 @@ exports.post = function(req, res) {
 	var bid = req.user.Business[0]._id;
 	var db = req.db;
 	var business = db.get('businesses');
-	console.log("bid: ---->>>>++++>>>: "+bid);
 	var disclosure = req.body.disclosure;
 
-console.log("disclosure:------------------------------------->>>>>>>>>>>>>>>>>>>> "+disclosure);
-	disclosure = disclosure.replace(/\r\n\r\n/g, '</p><p>');
-	disclosure = disclosure.replace(/\r\n/g, '</p><p>');
-	disclosure = '<p>' + disclosure + '</p>';
-	business.findAndModify({_id: bid},{
-		$set :{
-			disclosure: disclosure
-		}
-	});
-
-	res.render('business/setdisclosure', {
-			disclosure: req.body.disclosure
+	if (disclosure.length === 0)
+	{
+		res.render('business/setdisclosure', {
+			disclosure: req.body.disclosure,
+			alert: 'Disclosure field left empty, please fill it in.'
 		});
+	}
+	else
+	{
+		disclosure = disclosure.replace(/\r\n\r\n/g, '</p><p>');
+		disclosure = disclosure.replace(/\r\n/g, '</p><p>');
+		disclosure = '<p>' + disclosure + '</p>';
+		business.findAndModify({_id: bid},{
+			$set :{
+				disclosure: disclosure
+			}
+		});
+
+		res.render('business/setdisclosure', {
+			disclosure: req.body.disclosure,
+			edited: 'Disclosure agreement successfully saved!'
+		});
+	}
 }
